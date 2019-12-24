83e24d712a27d548f197374c94da7f4c8b4e8d6b
duongtrong1 <contact.trongdv@gmail.com>
2019-12-24 21:03:05 +0700

    Create project

---
+++ src/test/java/com/smartosc/ecommerce/clothes/ClothesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartosc.ecommerce.clothes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClothesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
